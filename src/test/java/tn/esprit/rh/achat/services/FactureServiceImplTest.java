e1efac71dc0751d9fb61ce3fa2f68e204f95812a
HanfiNardine <nardine.hanfi@esprit.tn>
2023-11-12 16:41:06 +0100

    JUnit

---
+++ src/test/java/tn/esprit/rh/achat/services/FactureServiceImplTest.java
@@ -0,0 +1,11 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FactureServiceImplTest {
+
+    @Test
+    void retrieveAllFactures() {
+    }
+}
